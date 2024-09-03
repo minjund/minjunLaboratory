d8fcfde574c833ba2a7a912e0515ced29b2fd740
minjund <mam060922@naver.com>
2024-09-03 23:33:21 +0900

    Initial commit

---
+++ src/main/java/com/minjun/minjunlaboratory/MinjunLaboratoryApplication.java
@@ -0,0 +1,13 @@
+package com.minjun.minjunlaboratory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinjunLaboratoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinjunLaboratoryApplication.class, args);
+    }
+
+}
